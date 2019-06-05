e194d398679a829032d4e9b9784b0ef8baa3e905
Nitril <athlonxp@poczta.fm>
2019-06-05 15:19:30 +0200

    Added 3rd package and finalized this project

---
+++ Lab/LoadUCIML_PL/LoadUCIML_PL_DB/dbo/Tables/CensusAdult.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[CensusAdult] (
+    [Age]           INT          NULL,
+    [Workclass]     VARCHAR (17) NULL,
+    [Fnlwgt]        INT          NULL,
+    [Education]     VARCHAR (13) NULL,
+    [EducationNum]  INT          NULL,
+    [MartialStatus] VARCHAR (22) NULL,
+    [Occupation]    VARCHAR (18) NULL,
+    [Relationship]  VARCHAR (15) NULL,
+    [Race]          VARCHAR (19) NULL,
+    [Sex]           VARCHAR (7)  NULL,
+    [CapitalGain]   INT          NULL,
+    [CapitalLoss]   INT          NULL,
+    [HoursPerWeek]  INT          NULL,
+    [NativeCountry] VARCHAR (27) NULL,
+    [Income]        VARCHAR (6)  NULL
+);
+
