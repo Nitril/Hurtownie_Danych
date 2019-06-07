4f585fa417253220ee5b3345c765196d3c4d5dd8
Nitril <athlonxp@poczta.fm>
2019-06-07 15:32:19 +0200

    before conditional split

---
+++ Lab/SCD2_PL/SCD2_PL_DB/dbo/Tables/DimStoreSCD2.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DimStoreSCD2] (
+    [StoreKey]         INT           NULL,
+    [GeographyKey]     INT           NULL,
+    [StoreManager]     INT           NULL,
+    [StoreName]        VARCHAR (100) NULL,
+    [StoreDescription] VARCHAR (300) NULL,
+    [Status]           VARCHAR (20)  NULL,
+    [OpenDate]         DATETIME      NULL,
+    [CloseDate]        DATETIME      NULL,
+    [PckgStartTime]    DATETIME      NULL,
+    [RowStartTime]     DATETIME      NULL
+);
+
